ea26b008f68fce3d562a9a7062b66a521a108153
ABOBA412 <kamvaspro@gmail.com>
2025-09-19 21:27:37 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -2296,10 +2296,10 @@ function Tab:AddDropdown(Configs)
 	Make("Corner", DropFrame) Make("Stroke", DropFrame) Make("Gradient", DropFrame, {Rotation = 60})
 
 	-- === SEARCH (маленькая квадратная кнопка в правом верхнем углу DropFrame; при открытии ездит вправо и раскрывает textbox влево) ===
-	local SEARCH_COLLAPSED_W = 130
-	local SEARCH_EXPANDED_W = 300
-	local SEARCH_H = 300
-	local SEARCH_GAP_TOP = 400
+	local SEARCH_COLLAPSED_W = 400
+	local SEARCH_EXPANDED_W = 600
+	local SEARCH_H = 500
+	local SEARCH_GAP_TOP = 600
 	local SEARCH_MOVE_RIGHT = 48 -- насколько вправо кнопка уедет при открытии (настраиваемо)
 
 	local SearchHolder = InsertTheme(Create("Frame", DropFrame, {
